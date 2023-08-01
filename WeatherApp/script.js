2d3dcede952da47000a9b9f1edbe328aa097b28d
Shahab <54299749+DoctorCnC@users.noreply.github.com>
2023-08-01 13:57:21 +0500

    Update script.js
    Fixed broken weather API

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,70 +1,59 @@
-// Fetch weather data from API
 async function getWeatherData(location) {
-    const apiKey = 'f28d9486987644fa35b964dcc8addf3a';
-    const url = `http://api.weatherstack.com/
-
-    /v1/forecast.json?key=${apiKey}&q=${location}&days=3`;
-    
     try {
-      const response = await fetch(url);
-      const data = await response.json();
-      return data;
+        const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&APPID=a1c07882a6b7d89e95550ea178aa6bce", { mode: "cors" });
+        if (!response.ok) {
+            throw new Error('Failed to fetch weather data');
+        }
+        const data = await response.json();
+        return data;
     } catch (error) {
-      console.error('Error fetching weather data:', error);
-      return null;
+        console.error('Error fetching weather data:', error.message);
+        return null;
     }
-  }
-  
-  // Process weather data and return relevant information
-  function processWeatherData(data) {
+}
+
+function processWeatherData(data) {
     if (!data) return null;
-    
-    const location = data.location.name;
-    const currentWeather = data.current.condition.text;
-    const currentTempC = data.current.temp_c;
-    const currentTempF = data.current.temp_f;
-  
+
+    const location = data.name;
+    const currentWeather = data.weather[0].description;
+    const currentTempF = data.main.temp;
+    const currentTempC = (data.main.temp * 9) / 5 + 32;
+
     return {
-      location,
-      currentWeather,
-      currentTempC,
-      currentTempF,
+        location,
+        currentWeather,
+        currentTempC,
+        currentTempF,
     };
-  }
-  document.getElementById('weatherForm').addEventListener('submit', async (event) => {
+}
+
+function displayWeatherInfo(weatherInfo) {
+    const weatherInfoElement = document.getElementById('weatherInfo');
+    if (weatherInfo) {
+        const { location, currentWeather, currentTempC, currentTempF } = weatherInfo;
+        const weatherHTML = `
+            <h2>${location}</h2>
+            <p>Weather: ${currentWeather}</p>
+            <p>Temperature: ${currentTempC}°C / ${currentTempF}°F</p>
+        `;
+        weatherInfoElement.innerHTML = weatherHTML;
+    } else {
+        weatherInfoElement.innerHTML = '<p>No weather data available</p>';
+    }
+}
+
+document.getElementById('weatherForm').addEventListener('submit', async (event) => {
     event.preventDefault();
-  
+
     const locationInput = document.getElementById('locationInput');
     const location = locationInput.value;
-    
-    // Clear input field after submission
-    locationInput.value = '';
-  
+    locationInput.value = ''; // Clear input field after submission
+
     // Fetch weather data and process it
     const weatherData = await getWeatherData(location);
     const weatherInfo = processWeatherData(weatherData);
-  
+
     // Display weather information on the webpage
     displayWeatherInfo(weatherInfo);
-  });
-  
-
-  function displayWeatherInfo(weatherInfo) {
-  if (!weatherInfo) {
-    // Handle the case when weather data is not available or an error occurred
-    console.log('Weather data not available');
-    return;
-  }
-
-  // Target DOM elements to display the weather information
-  const locationElement = document.getElementById('location');
-  const currentWeatherElement = document.getElementById('currentWeather');
-  const currentTempCElement = document.getElementById('currentTempC');
-  const currentTempFElement = document.getElementById('currentTempF');
-
-  // Update the content of the DOM elements with weather data
-  locationElement.textContent = weatherInfo.location;
-  currentWeatherElement.textContent = weatherInfo.currentWeather;
-  currentTempCElement.textContent = `${weatherInfo.currentTempC} °C`;
-  currentTempFElement.textContent = `${weatherInfo.currentTempF} °F`;
-}
+});
