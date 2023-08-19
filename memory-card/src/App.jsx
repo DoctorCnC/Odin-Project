b56368b7274b7ba51feff34bd26b9d17e41e78f1
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-08-19 14:19:05 +0500

    Create App.jsx

---
+++ memory-card/src/App.jsx
@@ -0,0 +1,12 @@
+import MemoryGame from './components/MemoryGame';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Memory Card Game</h1>
+      <MemoryGame />
+    </div>
+  );
+};
+
+export default App;
