2596138d8be22159258768973679afcb6c1f9abb
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-08-21 23:33:28 +0500

    Update app.js

--- Authentication-Express/app.js
+++ Authentication-Express/app.js
@@ -27,3 +27,34 @@ if (!match) {
 }
 
 
+
+const app = express();
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.render("index", { user: req.user });
+});
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.post("/sign-up", async (req, res, next) => {
+    try {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+        const result = await user.save();
+        res.redirect("/");
+    } catch (err) {
+        return next(err);
+    };
+});
+
+
